6a906a073a5b1e9217fb2846f2e07bff9b1b33e8
Andrew Dawes <andrewjamesdawes@gmail.com>
2024-08-16 12:02:28 +0000

    Add watch mode and change log event to json

--- src/migrations/2024-08-15T20:44:31-04:00.ts
+++ src/migrations/2024-08-15T20:44:31-04:00.ts
@@ -2,12 +2,13 @@ import { Kysely, sql } from "kysely";
 
 export async function up(db: Kysely<any>): Promise<void> {
 	await db.schema
-		.createTable("fencingToken")
-		.addColumn("id", "integer", (col) => col.primaryKey().autoIncrement())
-		.addColumn("used", "boolean", (col) => col.notNull())
+		.createTable("logRecord")
+        .addColumn("id", "integer", (col) => col.primaryKey().autoIncrement())
+        // Add an arbitrary JSON column
+        .addColumn("data", "json")
 		.execute();
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
-	await db.schema.dropTable("fencingToken").execute();
+	await db.schema.dropTable("logRecord").execute();
 }
